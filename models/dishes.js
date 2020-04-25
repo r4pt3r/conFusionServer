c93865d2ace90c2caf8eca9b40b11b996035e44d
r4pt3r <adarshk460@gmail.com>
2020-04-25 13:34:44 +0530

    Added models

---
+++ models/dishes.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;   
+
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        require: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true    
+});
+
+const dishSchema = new Schema({
+    name: {
+        type: String,
+        
+        unique: true
+    },
+    description: {
+        type: String,
+        
+    },
+    image: {
+        type: String,
+    
+    },
+    category: {
+        type: String
+
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Currency,
+        
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    },
+    comments:[commentSchema]
+}, {
+    timestamps: true
+});
+
+var Dishes = mongoose.model('Dish', dishSchema);
+
+module.exports = Dishes;
